f6641c70c4619173ac6a39026f3733fa756c5f28
annnnne0811 <annnnneflores2000@yahoo.com>
2025-01-22 20:52:29 +0200

    BMI lab 2

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -4,24 +4,37 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bmi.ui.theme.BMITheme
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.text.DecimalFormat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            BMITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+        setContent{
+            MaterialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()){ innerPadding ->
+                    Bmi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +44,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Bmi(modifier: Modifier = Modifier) {
+    var heightInput: String by remember { mutableStateOf("") }
+    var weightInput: String by remember { mutableStateOf("") }
+    val height = heightInput.toFloatOrNull() ?: 0.0f
+    val weight = weightInput.toIntOrNull() ?: 0
+    val formatter = DecimalFormat("0.00")
+    val bmi = if (weight > 0 && height > 0) formatter.format(weight / (height * height)) else 0.0f
+    Column {
+        Text(
+            text = "Body Mass Index",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp)
+
+        )
+        OutlinedTextField(
+            value = heightInput,
+            onValueChange = { heightInput = it.replace(',', '.') },
+            label = { Text("Height") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            modifier = Modifier.fillMaxWidth().padding(start = 8.dp, end = 8.dp)
+        )
+        OutlinedTextField(
+            value = weightInput,
+            onValueChange = { weightInput = it.replace(',', '.') },
+            label = { Text("Weight") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            ),
+            modifier = modifier.fillMaxWidth().padding(start = 8.dp, end = 8.dp)
+
+        )
+        Text(
+            text = "Body Mass Index is $bmi",
+            modifier = Modifier.fillMaxWidth().padding(start = 16.dp, top = 16.dp)
+
+        )
+
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BMITheme {
-        Greeting("Android")
+fun BmiPreview(){
+    BMITheme(){
+        Bmi()
     }
 }
